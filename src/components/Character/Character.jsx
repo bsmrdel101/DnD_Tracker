7ac70cfb8b31db7bc727ed7a184cf44f4a000e70
Bennett Smrdel <smrdelb@gmail.com>
2022-02-02 19:05:58 -0600

    transporting project

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function Character() {
     const dispatch = useDispatch();
 
+    const character = useSelector((store) => store.character);
+
     return (
-        <>
-            
+        <>  
+            <p>{character.name}</p>
+            <p>{character.class}</p>
         </>
     );
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,9 +5,11 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { useHistory } from 'react-router-dom';
 
 function Dashboard() {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const characters = useSelector((store) => store.character);
 
@@ -18,7 +20,11 @@ function Dashboard() {
   }, [])
 
   const handleSelection = (character) => {
-    console.log(character);
+    dispatch({
+      type: 'SET_CHARACTER',
+      payload: character
+    });
+    history.push('/character');
   }
 
   return (
@@ -26,7 +32,7 @@ function Dashboard() {
       <h1>Your Characters</h1>
       {characters.map((character, i) => {
         return (
-          <div className="character-selection-container"  key={i}>
+          <div className="character-selection-container" key={i}>
             <Card sx={{ maxWidth: 345, backgroundColor: '#e7e7e7' }} onClick={() => handleSelection(character)}>
                 <CardActionArea>
                     <CardContent>
