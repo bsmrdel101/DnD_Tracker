e90e3f44037dece1c7833a068f56e7e3d81deb02
Bennett Smrdel <smrdelb@gmail.com>
2022-02-05 17:55:53 -0600

    removed the add and subtract buttons for number inputs

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -10,6 +10,7 @@ import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox
 import { Box } from '@mui/system';
 import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
 import ShieldIcon from '@mui/icons-material/Shield';
+import { Grid } from '@mui/material';
 
 import './Character.css'
 
@@ -37,111 +38,115 @@ function Character() {
             {selectedCharacter.map((character) => {
                 return (
                     <div key={character.id} className='container'>
-                        {/* Basic character info */}
-                        <Card sx={{ maxWidth: 345, backgroundColor: '#e7e7e7' }}>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    <Avatar sx={{ width: 56, height: 56 }} alt="Avatar" src="orc_pic.png" className="avatar" />
-                                    {character.name}
-                                    <Typography sx={{ fontSize: 18 }} variant="p" component="div">
-                                        Level {character.level}
-                                    </Typography>
-                                </Typography>
-                                <Typography variant="p" component="div">
-                                    <span className='bold-text'>Race:</span> {character.race}
-                                </Typography>
-                                <Typography variant="p" component="div">
-                                    <span className='bold-text'>Class:</span> {character.class}
-                                </Typography>
-                                <Typography variant="p" component="div">
-                                    <span className='bold-text'>Background:</span> {character.background}
-                                </Typography>
-                                <Typography sx={{ paddingTop: 1 }} variant="p" component="div">
-                                    <span className='bold-text'>AC:</span> {character.ac}
-                                </Typography>
-                                <Typography variant="p" component="div">
-                                    <span className='bold-text'>Proficiency Bonus:</span> +{character.prof_bonus}
-                                </Typography>
-                                <Typography variant="p" component="div">
-                                    <span className='bold-text'>Initiative:</span> {character.initiative >= 0 && '+'}{character.initiative}
-                                </Typography>
-                                <Typography variant="p" component="div">
-                                    <span className='bold-text'>Movement:</span> {character.movement}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        {/* Health management */}
-                        <Card sx={{ maxWidth: 345, backgroundColor: '#e7e7e7', marginTop: 2 }}>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div" sx={{ fontSize: 30 }}>
-                                    <FavoriteIcon id='heart-icon' /> {healthReducer.health} 
-                                    <FavoriteIcon id='blue-heart-icon' /> {healthReducer.temp_health}    
-                                    {/* Heal */}
-                                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                                        <button 
-                                            id='add-hp-btn'
-                                            onClick={() => {
-                                                dispatch({type:'HEAL', payload: heal});
-                                                setHeal('');
-                                            }}>
-                                                <LocalHospitalIcon 
-                                                    id='add-hp-icon'
-                                                    sx={{ color: '#119711', mr: 1, my: 0.5 }} 
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={4}>
+                                {/* Basic character info */}
+                                <Card sx={{ maxWidth: 345, backgroundColor: '#f3c6c6' }}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            <Avatar sx={{ width: 56, height: 56 }} alt="Avatar" src="orc_pic.png" className="avatar" />
+                                            {character.name}
+                                            <Typography sx={{ fontSize: 18 }} variant="p" component="div">
+                                                Level {character.level}
+                                            </Typography>
+                                        </Typography>
+                                        <Typography variant="p" component="div">
+                                            <span className='bold-text'>Race:</span> {character.race}
+                                        </Typography>
+                                        <Typography variant="p" component="div">
+                                            <span className='bold-text'>Class:</span> {character.class}
+                                        </Typography>
+                                        <Typography variant="p" component="div">
+                                            <span className='bold-text'>Background:</span> {character.background}
+                                        </Typography>
+                                        <Typography sx={{ paddingTop: 1 }} variant="p" component="div">
+                                            <span className='bold-text'>AC:</span> {character.ac}
+                                        </Typography>
+                                        <Typography variant="p" component="div">
+                                            <span className='bold-text'>Proficiency Bonus:</span> +{character.prof_bonus}
+                                        </Typography>
+                                        <Typography variant="p" component="div">
+                                            <span className='bold-text'>Initiative:</span> {character.initiative >= 0 && '+'}{character.initiative}
+                                        </Typography>
+                                        <Typography variant="p" component="div">
+                                            <span className='bold-text'>Movement:</span> {character.movement}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                                {/* Health management */}
+                                <Card sx={{ maxWidth: 345, backgroundColor: '#f3c6c6', marginTop: 2 }}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div" sx={{ fontSize: 30 }}>
+                                            <FavoriteIcon id='heart-icon' /> {healthReducer.health} 
+                                            <FavoriteIcon id='blue-heart-icon' /> {healthReducer.temp_health}    
+                                            {/* Heal */}
+                                            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                                                <button 
+                                                    id='add-hp-btn'
+                                                    onClick={() => {
+                                                        dispatch({type:'HEAL', payload: heal});
+                                                        setHeal('');
+                                                    }}>
+                                                        <LocalHospitalIcon 
+                                                            id='add-hp-icon'
+                                                            sx={{ color: '#119711', mr: 1, my: 0.5 }} 
+                                                        />
+                                                </button>
+                                                <input 
+                                                    id='add-hp'
+                                                    placeholder="Heal"
+                                                    type="number"
+                                                    value={heal}
+                                                    onChange={(event) => setHeal(event.target.value)}
                                                 />
-                                        </button>
-                                        <input 
-                                            id='add-hp'
-                                            placeholder="Heal"
-                                            type="number"
-                                            value={heal}
-                                            onChange={(event) => setHeal(event.target.value)}
-                                        />
-                                    </Box>  
-                                    {/* Damage */}
-                                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                                        <button 
-                                            id='dmg-hp-btn' 
-                                            onClick={() => {
-                                                dispatch({type:'DAMAGE', payload: damage});
-                                                setDamage('');
-                                            }}>
-                                                <IndeterminateCheckBoxIcon 
-                                                    id='dmg-hp-icon' 
-                                                    sx={{ color: 'red', mr: 1, my: 0.5 }} 
+                                            </Box>  
+                                            {/* Damage */}
+                                            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                                                <button 
+                                                    id='dmg-hp-btn' 
+                                                    onClick={() => {
+                                                        dispatch({type:'DAMAGE', payload: damage});
+                                                        setDamage('');
+                                                    }}>
+                                                        <IndeterminateCheckBoxIcon 
+                                                            id='dmg-hp-icon' 
+                                                            sx={{ color: 'red', mr: 1, my: 0.5 }} 
+                                                        />
+                                                </button>
+                                                <input 
+                                                    id='dmg-hp'
+                                                    placeholder="Damage" 
+                                                    type="number"
+                                                    value={damage}
+                                                    onChange={(event) => setDamage(event.target.value)}
                                                 />
-                                        </button>
-                                        <input 
-                                            id='dmg-hp'
-                                            placeholder="Damage" 
-                                            type="number"
-                                            value={damage}
-                                            onChange={(event) => setDamage(event.target.value)}
-                                        />
-                                    </Box>  
-                                    {/* Add temp health */}
-                                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                                        <button 
-                                            id='temp-hp-btn' 
-                                            onClick={() => {
-                                                dispatch({type:'ADD_TEMP', payload: tempHealth});
-                                                setTempHealth('');
-                                            }}>
-                                                <ShieldIcon 
-                                                    id='temp-hp-icon' 
-                                                    sx={{ color: '#4343bf', mr: 1, my: 0.5 }} 
+                                            </Box>  
+                                            {/* Add temp health */}
+                                            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                                                <button 
+                                                    id='temp-hp-btn' 
+                                                    onClick={() => {
+                                                        dispatch({type:'ADD_TEMP', payload: tempHealth});
+                                                        setTempHealth('');
+                                                    }}>
+                                                        <ShieldIcon 
+                                                            id='temp-hp-icon' 
+                                                            sx={{ color: '#4343bf', mr: 1, my: 0.5 }} 
+                                                        />
+                                                </button>
+                                                <input 
+                                                    id='temp-hp'
+                                                    placeholder="Temporary Health" 
+                                                    type="number"
+                                                    value={tempHealth}
+                                                    onChange={(event) => setTempHealth(event.target.value)}
                                                 />
-                                        </button>
-                                        <input 
-                                            id='temp-hp'
-                                            placeholder="Temporary Health" 
-                                            type="number"
-                                            value={tempHealth}
-                                            onChange={(event) => setTempHealth(event.target.value)}
-                                        />
-                                    </Box>  
-                                </Typography>
-                            </CardContent>
-                        </Card>
+                                            </Box>  
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        </Grid>
                     </div>
                 );
             })}
