0b2490c81dc1120f52efdcea44d6cd7551689c04
Bennett Smrdel <smrdelb@gmail.com>
2022-02-05 09:10:32 -0600

    created a back button for the characters page, in case the user reloads and there's no character data

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function Character() {
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const selectedCharacter = useSelector((store) => store.selectedCharacterReducer);
 
     return (
-        <>  {selectedCharacter.map((character) => {
+        <>  
+            {selectedCharacter.length === 0 &&
+                <button onClick={() => history.push('/dashboard')}>Back</button>
+            }
+            {selectedCharacter.map((character) => {
                 return (
                     <div key={character.id}>
                         <p>{character.name}</p>
