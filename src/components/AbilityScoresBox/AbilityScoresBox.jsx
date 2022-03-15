a1869e08a8955925afb515690e1b8bf450c79498
Bennett Smrdel <smrdelb@gmail.com>
2022-03-15 17:31:57 -0500

    modifier numbers are displaying on stats

--- src/components/AbilityScoresBox/AbilityScoresBox.jsx
+++ src/components/AbilityScoresBox/AbilityScoresBox.jsx
@@ -7,6 +7,30 @@ import Typography from '@mui/material/Typography';
 import './AbilityScoresBox.css';
 
 function AbilityScoresBox({ character }) {
+    const getModifier = (type) => {
+        let baseAbility = getAbilityScore(type);
+        let calculatedModifier = Math.floor((baseAbility - 10) / 2);
+        return calculatedModifier;
+    }
+
+    const getAbilityScore = (type) => {
+        switch (type) {
+            case 'str':
+                return character.str;
+            case 'dex':
+                return character.dex;
+            case 'con':
+                return character.con;
+            case 'wis':
+                return character.wis;
+            case 'int':
+                return character.int;
+            case 'char':
+                return character.char;
+            default:
+                break;
+        }
+    }
 
     return (
         <Card sx={{ backgroundColor: '#f3c6c6', borderRadius: 4 }}>
@@ -24,6 +48,9 @@ function AbilityScoresBox({ character }) {
                             <Typography gutterBottom variant="h6" component="div">
                                 {character.str}
                             </Typography>
+                            <Typography gutterBottom variant="p" component="div" className='stat-mod'>
+                                {getModifier('str')} 
+                            </Typography>
                         </div>
                         {/* Dex */}
                         <div className='ability-score-box'>
@@ -33,6 +60,9 @@ function AbilityScoresBox({ character }) {
                             <Typography gutterBottom variant="h6" component="div">
                                 {character.dex}
                             </Typography>
+                            <Typography gutterBottom variant="p" component="div" className='stat-mod'>
+                                {getModifier('dex')} 
+                            </Typography>
                         </div>
                         {/* Con */}
                         <div className='ability-score-box'>
@@ -41,6 +71,9 @@ function AbilityScoresBox({ character }) {
                             </Typography>
                             <Typography gutterBottom variant="h6" component="div">
                                 {character.con}
+                            </Typography>
+                            <Typography gutterBottom variant="p" component="div" className='stat-mod'>
+                                {getModifier('con')} 
                             </Typography>
                         </div>
                     </div>
@@ -53,6 +86,9 @@ function AbilityScoresBox({ character }) {
                             <Typography gutterBottom variant="h6" component="div">
                                 {character.int}
                             </Typography>
+                            <Typography gutterBottom variant="p" component="div" className='stat-mod'>
+                                {getModifier('int')} 
+                            </Typography>
                         </div>
                         {/* Wis */}
                         <div className='ability-score-box'>
@@ -62,6 +98,9 @@ function AbilityScoresBox({ character }) {
                             <Typography gutterBottom variant="h6" component="div">
                                 {character.wis}
                             </Typography>
+                            <Typography gutterBottom variant="p" component="div" className='stat-mod'>
+                                {getModifier('wis')} 
+                            </Typography>
                         </div>      
                         {/* Char */}
                         <div className='ability-score-box'>
@@ -70,6 +109,9 @@ function AbilityScoresBox({ character }) {
                             </Typography>
                             <Typography gutterBottom variant="h6" component="div">
                                 {character.char}
+                            </Typography>
+                            <Typography gutterBottom variant="p" component="div" className='stat-mod'>
+                                {getModifier('char')} 
                             </Typography>
                         </div>                                    
                     </div>                                            
